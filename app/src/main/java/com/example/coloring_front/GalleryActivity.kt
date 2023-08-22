5d8fa2b8a426e23ffb7375f9f07bf32bc12cebc8
k-oyun <113766051+k-oyun@users.noreply.github.com>
2023-08-22 16:03:19 +0900

    modify

--- app/src/main/java/com/example/coloring_front/GalleryActivity.kt
+++ app/src/main/java/com/example/coloring_front/GalleryActivity.kt
@@ -1,11 +1,28 @@
 package com.example.coloring_front
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class GalleryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
+
+
+        setContentView(R.layout.activity_main)
+
+        val gallery = intent.getIntExtra("gallery",0)
+
+        Log.d("gallery","" + gallery)
+
+        val resultGallery = Intent()
+
+        resultGallery.putExtra("result",resultGallery)
+
+        setResult(Activity.RESULT_OK,resultGallery)
+        finish()
     }
 }
--- app/src/main/java/com/example/coloring_front/MainActivity.kt
+++ app/src/main/java/com/example/coloring_front/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.coloring_front
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val red : ImageView = findViewById(R.id.red)
+        val green : ImageView = findViewById(R.id.green)
+        val blue : ImageView = findViewById(R.id.blue)
+        val all : ImageView = findViewById(R.id.all)
+        val picture : Button = findViewById(R.id.picture)
+
+        red.setOnClickListener{
+
+        }
+
+        green.setOnClickListener{
+
+        }
+
+        blue.setOnClickListener{
+
+        }
+
+        all.setOnClickListener{
+            val gallery = Intent(this@MainActivity, GalleryActivity::class.java)
+            gallery.apply {
+                this.putExtra("gallery",0)
+            }
+
+
+        }
+
     }
 }
