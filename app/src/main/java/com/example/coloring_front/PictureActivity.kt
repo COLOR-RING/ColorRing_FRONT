adc182a2b55f4c9815ecca95dc4690a76dd96d23
k-oyun <113766051+k-oyun@users.noreply.github.com>
2023-08-22 15:06:09 +0900

    modify activity_picture.xml

--- app/src/main/java/com/example/coloring_front/PictureActivity.kt
+++ app/src/main/java/com/example/coloring_front/PictureActivity.kt
@@ -3,7 +3,7 @@ package com.example.coloring_front
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class activity_picture : AppCompatActivity() {
+class PictureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture)
