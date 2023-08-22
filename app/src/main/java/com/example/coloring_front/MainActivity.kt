727ab01c9ecf1409c072ebaad94282111cc9950a
yeopyeop-82 <a01055143647@gmail.com>
2023-08-22 17:07:50 +0900

    gallery

--- app/src/main/java/com/example/coloring_front/MainActivity.kt
+++ app/src/main/java/com/example/coloring_front/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import com.example.coloring_front.databinding.ActivityIntroBinding
 import com.example.coloring_front.databinding.ActivityMainBinding
 
@@ -15,6 +18,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.picture.setOnClickListener {
+            //갤러리 호출
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            activityResult.launch(intent)
+        }
 
         val red : ImageView = findViewById(R.id.red)
         val green : ImageView = findViewById(R.id.green)
@@ -43,6 +53,21 @@ class MainActivity : AppCompatActivity() {
 
         }
         
+    }
+
+    private val activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+
+        //결과 코드 OK , 결가값 null 아니면
+        if(it.resultCode == RESULT_OK && it.data != null){
+            //값 담기
+            val uri  = it.data!!.data
+
+            //화면에 보여주기
+            Glide.with(this)
+                .load(uri) //이미지
+                .into(binding.galleryPic) //보여줄 위치
+        }
     }
 }
 
